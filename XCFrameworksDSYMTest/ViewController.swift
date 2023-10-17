d0a40bfc23849e6a38f25204f1782a654a37bae2
Nikita Borodulin <nborodulin.dev@gmail.com>
2023-10-17 09:45:26 +0300

    create project

---
+++ XCFrameworksDSYMTest/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Created by Nikita Borodulin on 17.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
